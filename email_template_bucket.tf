3e8c10233611c2eef74d99064d694eddd8ff6ec0
Orest <orest.kanyuk@temy.co>
2022-07-15 14:06:24 +0300

    apply_server_side_encryption_by_default

--- email_template_bucket.tf
+++ email_template_bucket.tf
@@ -17,6 +17,18 @@ resource "aws_s3_bucket" "email_templates_bucket" {
   bucket        = local.email_templates_bucket_name
   force_destroy = true
 
+  versioning {
+    enabled = true   
+  }
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = merge(var.tags, { Name = local.email_templates_bucket_name })
 }
 #--------------------------------------------------------------
